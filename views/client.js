e888bee233105fdf5278abc2560eba939bbbb1b0
Saramma Varghese <1028285@questgedctvm.com>
2020-05-20 13:52:51 +0530

    initial commit

---
+++ views/client.js
@@ -0,0 +1,10 @@
+
+// const button = document.getElementById('myButton');
+// button.addEventListener('click', function(e) {
+//   alert('Not Available...');
+// });
+
+showAlert()
+{
+    alert("wdqwq");
+}
